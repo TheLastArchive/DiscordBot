64c43dbc05559c5feb09268f8541577e3f4603bf
TheLastArchive <iarchive@hotmail.com>
2022-11-02 12:48:28 +0000

    Refactored deals command

--- commands/deals.js
+++ commands/deals.js
@@ -1,7 +1,7 @@
 
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const axios = require('axios')
-const cheerio = require('cheerio')
+const cheerio = require('cheerio');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
@@ -13,17 +13,20 @@ module.exports = {
 
         const deals = await get_deals();
         const embed = createEmbed(deals);
+        console.log("Sending embed")
         await interaction.reply({ embeds: [embed] });
     },
 };
 
 
 async function get_deals() {
-    
+    console.log("Sending request to gg.deals")
     return await axios("https://gg.deals")
     .then(res => {
+        console.log("Request received")
         const deals = []
         const $ = cheerio.load(res.data)
+        console.log("Scraping HTML")
         $('.deal-list-item', res.data).each(function(index, element) {
             const url = $(element).find('a').attr('href')
             const title = $('.main-wrap', $(element)).find('a').attr('title')
@@ -43,58 +46,55 @@ async function get_deals() {
 
 function createEmbed(deals) {
 
-    //f_ = formatted
-    const f_data = formatData(deals)
+    const data = formatData(deals)
 
     console.log("Creating embed");
     const embed = new MessageEmbed()
         .setTitle('GG.DEALS')
         .setColor('#23222f')
         .addFields(
-            { name: "New Deals", value: f_data.f_new_deals, inline: true},
-            { name: "Price", value: f_data.f_new_deals_price, inline: true},
+            { name: "New Deals", value: data.new_deals.titles, inline: true},
+            { name: "Price", value: data.new_deals.prices, inline: true},
+            { name: '\u200b', value: '\u200b', inline: true}, //Embeds rollover every 3 fields, add a blank 3rd field for formatting
+
+            { name: "Best Deals", value: data.best_deals.titles, inline: true},
+            { name: "Price", value: data.best_deals.prices, inline: true},
             { name: '\u200b', value: '\u200b', inline: true},
-            { name: "Best Deals", value: f_data.f_best_deals, inline: true},
-            { name: "Price", value: f_data.f_best_deals_price, inline: true},
-            { name: '\u200b', value: '\u200b', inline: true},
-            { name: "Historical Lows", value: f_data.f_hist_lows, inline: true},
-            { name: "Price", value: f_data.f_hist_lows_price, inline: true},
+
+            { name: "Historical Lows", value: data.hist_lows.titles, inline: true},
+            { name: "Price", value: data.hist_lows.prices, inline: true},
             { name: '\u200b', value: '\u200b', inline: true})
         
     return embed;
 }
 
 function formatData(deals) {
-    const new_deals = deals.slice(0, 10);
-    const best_deals = deals.slice(10, 20);
-    const hist_lows = deals.slice(-10);
+    //30 deal objects, 10 per category so split the list into 3 equal lists
+    const sliced_deals = []
+    sliced_deals.push(deals.slice(0, 10))
+    sliced_deals.push(deals.slice(10, 20))
+    sliced_deals.push(deals.slice(-10))
 
-    //f_ = formatted
-    let f_new_deals = ""
-    let f_new_deals_price = ""
-    new_deals.forEach(element => {
-        f_new_deals += `[${element.title}](https://gg.deals${element.url})\n`
-        f_new_deals_price += `${element.price}\n`
-    });
-    let f_best_deals = ""
-    let f_best_deals_price = ""
-    best_deals.forEach(element => {
-        f_best_deals += `[${element.title}](https://gg.deals${element.url})\n`
-        f_best_deals_price += `${element.price}\n`
-    });
-    let f_hist_lows = ""
-    let f_hist_lows_price = ""
-    hist_lows.forEach(element => {
-        f_hist_lows += `[${element.title}](https://gg.deals${element.url})\n`
-        f_hist_lows_price += `${element.price}\n`
-    });
+    data = {
+        "new_deals": [],
+        "best_deals": [],
+        "hist_lows": []
+    }
 
-    return ({
-        f_new_deals,
-        f_new_deals_price,
-        f_best_deals,
-        f_best_deals_price,
-        f_hist_lows,
-        f_hist_lows_price
-    })
+    for (i = 0; i < 3; i++) {
+        let deals_titles = ""
+        let deals_prices = ""
+
+        sliced_deals[i].forEach(deal => {
+            deals_titles += `[${deal.title}](https://gg.deals${deal.url})\n`
+            deals_prices += `${deal.price}\n`
+        })
+
+        //Access object values by index rather than key
+        data[Object.keys(data)[i]] = {
+            "titles": deals_titles,
+            "prices": deals_prices
+        }
+    }
+    return data
 }
